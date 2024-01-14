19c0c8c9f134e519084bc01c735c6e84b773ec49
cbiryol4 <cbiryol4@bloomberg.net>
2024-01-14 23:17:28 +0000

    Ex.4

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react"
+
+const getDocPic = async () => {
+  const response = await fetch('https://dog.ceo/api/breeds/image/random')
+  const dog = await response.json()
+  return dog.message
+}
+
 export default function DogPics () {
+  const [dogPic, setDogPic] = useState('');
+
   // API: https://dog.ceo/dog-api/
+  useEffect(() => {
+    getDocPic().then(pic => setDogPic(pic))
+  }, [])
   return (
     <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+      <img src={dogPic} alt='dog' />
+      <button onClick={async e => setDogPic(await getDocPic())} >🐶</button>
     </div>
   )
 }
